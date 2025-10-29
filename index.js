b2e45fa404628819a8a6597d9942e1a79a588116
Stuart Frew <stuart.frew@hapara.com>
2025-10-29 20:55:20 +1300

    WIP

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const width = "100%";
 const height = "100%";
 
+expansions = ["Kingsport", "Dunwich", "Innsmouth"];
+
 axios
   .get("src/main/resources/data/locations.json")
   .then((response) => {
@@ -15,6 +17,12 @@ axios
         };
       }),
     );
+
+    const container = document.getElementById("board");
+    const width = container.clientWidth;
+    const height = container.clientHeight;
+
+    console.log("width:", width, " height:", height);
 
     const neighbourhoodNodes = board.neighborhoods.map((n) => {
       return {
@@ -34,46 +42,57 @@ axios
       });
     });
 
-    const neighborhoodLinks = board.relationships.flatMap( r => {
-      return r.to.map(t => {
+    const neighborhoodLinks = board.relationships.flatMap((r) => {
+      return r.to.map((t) => {
         return {
           source: r.from,
           target: t,
-          type: "neighborhood"
-        }
-      })
-    })
+          type: "neighborhood",
+        };
+      });
+    });
 
-    const links = locationLinks.concat(neighborhoodLinks)
+    const links = locationLinks.concat(neighborhoodLinks);
 
     const color = d3.scaleOrdinal(d3.schemeCategory10);
 
     // Create a simulation with several forces.
     const simulation = d3
       .forceSimulation(nodes)
-      .force("link", d3.forceLink(links).id(d => d.id ).distance(d => {
-        if ( d.type == "neighborhood" ) {
-          return 100
-        } else {
-          return 40
-        }
-      }))
+      .force(
+        "link",
+        d3
+          .forceLink(links)
+          .id((d) => d.id)
+          .distance((d) => {
+            console.log("d", d);
+            if (
+              (expansions.includes(d.source.id) ||
+                expansions.includes(d.target.id)) &&
+              d.source != "location"
+            ) {
+              return 500;
+            } else if (d.source.type == "neighborhood") {
+              return 100;
+            } else {
+              return 50;
+            }
+          }),
+      )
       .force("charge", d3.forceManyBody().strength(-8))
       .force("collide", d3.forceCollide(10))
       .force("x", d3.forceX())
-      .force("center", d3.forceCenter(150, 150))
+      .force("center", d3.forceCenter(width / 2, height / 2))
       .force("y", d3.forceY());
-    
 
     // Create the SVG container.
     const svg = d3
       .create("svg")
-      .attr("width", "100%")
-      .attr("height", "100%")
+      .attr("width", width)
+      .attr("height", height)
       .attr("preserveAspectRatio", "xMinYMin meet")
-      .attr("viewBox", "-200, -200, 500, 500")
+      .attr("viewBox", [0, 0, width, height])
       .attr("style", "max-width: 100%; height: auto; font: 12px sans-serif;");
- 
 
     // Add a line for each link, and a circle for each node.
     const link = svg
@@ -92,21 +111,22 @@ axios
       .selectAll(".node")
       .data(nodes)
       .enter()
-      .append("g")
+      .append("g");
 
-
-    node.append("circle")
+    node
+      .append("circle")
       .attr("r", 5)
-      .attr("fill", (d) => color(getColorIndex(d, board)))
-     
-    node.append("text")
-    .attr("x", 12)   // offset to the right of the circle
-    .attr("y", 3)    // vertical centering
-    .attr("stroke-width", 0.5)
-    .text(d => d.id)
-    .attr("stroke", (d) => color(getColorIndex(d, board)))
-    .attr("class", "name")
-  
+      .attr("fill", (d) => color(getColorIndex(d, board)));
+
+    node
+      .append("text")
+      .attr("x", 12) // offset to the right of the circle
+      .attr("y", 3) // vertical centering
+      .attr("stroke-width", 0.5)
+      .text((d) => d.id)
+      .attr("stroke", (d) => color(getColorIndex(d, board)))
+      .attr("class", "name");
+
     // Add a drag behavior.
     node.call(
       d3
@@ -124,7 +144,7 @@ axios
         .attr("x2", (d) => d.target.x)
         .attr("y2", (d) => d.target.y);
 
-      node.attr("transform", d => `translate(${d.x},${d.y})`);
+      node.attr("transform", (d) => `translate(${d.x},${d.y})`);
     });
 
     document.getElementById("board").appendChild(svg.node());
@@ -155,12 +175,14 @@ function dragended(event) {
 function getColorIndex(node, board) {
   index = board.neighborhoods.findIndex((n) => {
     if (node.type == "location") {
-      return n.locations.find( l => { return l.name == node.id });
+      return n.locations.find((l) => {
+        return l.name == node.id;
+      });
     } else {
       return n.name == node.id;
     }
-  })
-  return index
+  });
+  return index;
 }
 // When this cell is re-run, stop the previous simulation. (This doesn’t
 // really matter since the target alpha is zero and the simulation will
